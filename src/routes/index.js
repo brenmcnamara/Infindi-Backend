4084ec051a31bab57ac0e90bdd30ced96dfc4d31
Brendan McNamara <brendan.d.mcnamara@vanderbilt.edu>
2017-11-27 20:47:31 -0800

    Renames ui to debug and adds environment check before including

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,18 +3,21 @@
 import auth, { initialize as initializeAuth } from './auth';
 import express from 'express';
 import plaid, { initialize as initializePlaid } from './plaid';
-import ui, { initialize as initializeUI } from './ui';
+import debug, { initialize as initializeDebug } from './debug';
 
 const router = express.Router();
-
-router.use('/auth', auth);
-router.use('/plaid', plaid);
-router.use('/ui', ui);
 
 export default router;
 
 export function initialize(): void {
   initializeAuth();
   initializePlaid();
-  initializeUI();
+  initializeDebug();
+
+  router.use('/auth', auth);
+  router.use('/plaid', plaid);
+
+  if (process.env.INCLUDE_DEBUG_ROUTES === 'true') {
+    router.use('/debug', debug);
+  }
 }
