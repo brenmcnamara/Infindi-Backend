71ad9450591dbf8ee9a2eb96a40f24cb11714f03
Brendan McNamara <brendan.d.mcnamara@vanderbilt.edu>
2017-11-28 21:56:52 -0800

    Adds firebase login for debugging

--- src/routes/debug.js
+++ src/routes/debug.js
@@ -4,10 +4,19 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/plaid', (req, res) => {
-  res.render('plaid');
-});
-
 export default router;
 
-export function initialize(): void {}
+export function initialize(): void {
+  router.get('/plaid', (req, res) => {
+    res.render('plaid');
+  });
+
+  router.get('/login', (req, res) => {
+    res.render('firebase-login', {
+      API_KEY: process.env.FIREBASE_WEB_API_KEY,
+      AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+      DB_URL: process.env.FIREBASE_DB_URL,
+      STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    });
+  });
+}
