ee12818f607e4d46d1c7e43fe02922273008e210
Brendan McNamara <brendan.d.mcnamara@vanderbilt.edu>
2017-11-30 08:17:01 -0800

    Creates script for fetching up-to-date item id

--- src/routes/debug.js
+++ src/routes/debug.js
@@ -10,13 +10,4 @@ export function initialize(): void {
   router.get('/plaid', (req, res) => {
     res.render('plaid');
   });
-
-  router.get('/login', (req, res) => {
-    res.render('firebase-login', {
-      API_KEY: process.env.FIREBASE_WEB_API_KEY,
-      AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
-      DB_URL: process.env.FIREBASE_DB_URL,
-      STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
-    });
-  });
 }
