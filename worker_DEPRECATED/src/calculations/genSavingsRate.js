08d9b620182ad38eeb8cbde0cfc6ebb133bfde5a
Brendan McNamara <brendan.d.mcnamara@vanderbilt.edu>
2018-02-20 20:34:46 -0800

    Deletes legacy directories

--- worker_DEPRECATED/src/calculations/genSavingsRate.js
+++
@@ -1,9 +0,0 @@
-/* @flow */
-
-import type { ID, ZeroToOneInclusive } from 'common/types/core';
-
-async function genSavingsRate(userID: ID): Promise<ZeroToOneInclusive> {
-  return Promise.resolve(0);
-}
-
-export default genSavingsRate;
