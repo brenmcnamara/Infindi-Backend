e61bb86899acf4932d40e7026fc77c0196cf5901
Brendan McNamara <brendan.d.mcnamara@vanderbilt.edu>
2018-02-26 20:16:40 -0800

    Updates jobs to use statuses

--- src/listen-for-jobs.js
+++ src/listen-for-jobs.js
@@ -4,10 +4,15 @@ import invariant from 'invariant';
 import request from 'request';
 
 import { ERROR, INFO } from './log-utils';
-import { isExpired, listenForJobs } from 'common/lib/models/Job';
+import {
+  listenForJobs,
+  updateJobStatus,
+  genUpdateJob,
+  getTimeTillRunMillis,
+} from 'common/lib/models/Job';
 
 import type { ID } from 'common/types/core';
-import type { Job, JobSchedule, Subscription } from 'common/lib/models/Job';
+import type { Job, Subscription } from 'common/lib/models/Job';
 
 const jobCollection: { [id: ID]: Job } = {};
 const jobSubscriptions: { [id: ID]: Subscription } = {};
@@ -19,34 +24,37 @@ export function initialize(): void {
 function onNewJobs(jobs: Array<Job>): void {
   INFO('JOB-RUNNER', `Receiving ${jobs.length} new jobs(s)`);
 
-  const modifiedJobs = jobs.filter(
-    job =>
-      !isExpired(job) &&
-      jobCollection[job.id] &&
-      isLegalJobChange(jobCollection[job.id], job),
-  );
-  const newJobs = jobs.filter(job => !isExpired(job) && !jobCollection[job.id]);
-  const corruptJobs = jobs.filter(
-    job =>
-      !isExpired(job) &&
-      jobCollection[job.id] &&
-      !isLegalJobChange(jobCollection[job.id], job),
-  );
+  let newJobsCount = 0;
+  let modifiedJobsCount = 0;
 
-  corruptJobs.forEach(job => {
-    ERROR('JOB-RUNNER', `Job ${job.id} has been corrupted`);
-  });
+  for (const job of jobs) {
+    // Note that while we are running jobs and updating their statuses, we
+    // will get updates of those jobs here. Jobs that are running already cannot
+    // start running, we will ignore them.
+    const canRun = getTimeTillRunMillis(job) !== null;
+    if (!canRun) {
+      continue;
+    }
+
+    const doesExist = Boolean(jobCollection[job.id]);
+    if (doesExist) {
+      if (isLegalJobChange(jobCollection[job.id], job)) {
+        ++modifiedJobsCount;
+      } else {
+        ERROR('JOB-RUNNER', `Job ${job.id} has been corrupted`);
+      }
+    } else {
+      const subscription = scheduleJob(job);
+      jobCollection[job.id] = job;
+      jobSubscriptions[job.id] = subscription;
+      ++newJobsCount;
+    }
+  }
 
   INFO(
     'JOB-RUNNER',
-    `${newJobs.length} new job(s). ${modifiedJobs.length} modified job(s).`,
+    `${newJobsCount} new job(s). ${modifiedJobsCount} modified job(s).`,
   );
-  modifiedJobs.forEach(job => (jobCollection[job.id] = job));
-  newJobs.forEach(job => {
-    jobCollection[job.id] = job;
-    const subscription = scheduleJob(job.id, job.schedule);
-    jobSubscriptions[job.id] = subscription;
-  });
 }
 
 // -----------------------------------------------------------------------------
@@ -78,36 +86,34 @@ function isLegalJobChange(originalJob: Job, modifiedJob): bool {
   );
 }
 
-function scheduleJob(jobID: ID, schedule: JobSchedule): Subscription {
+function scheduleJob(job: Job): Subscription {
   invariant(
-    !jobSubscriptions[jobID],
+    !jobSubscriptions[job.id],
     'Trying to schedule a job that is already scheduled',
   );
-  invariant(
-    jobCollection[jobID],
-    'Trying to schedule a job that cannot be found',
-  );
-  invariant(
-    !isExpired(jobCollection[jobID]),
-    'Trying to schedule a job that is expired',
-  );
-  invariant(
-    schedule.recurringType === 'ONCE',
-    'Scheduling jobs only supports schedule type ONCE',
-  );
-  const runAtMillis = schedule.runAt;
-  const nowMillis = Date.now();
-  const timeout = setTimeout(() => {
+  const timeTillRunMillis = getTimeTillRunMillis(job);
+  const timeout = setTimeout(async () => {
     INFO('JOB-RUNNER', 'Executing job');
-    const job = jobCollection[jobID];
-    invariant(job, 'Trying to run a job that cannot be found');
-    // TODO: Should I add job statuses for when jobs complete. Don't need
-    // this at the moment, may need it later.
-    genRunJob(job).catch(error => {
+    let updatedJob = jobCollection[job.id];
+    invariant(updatedJob, 'Trying to run a job that cannot be found');
+    updatedJob = updateJobStatus(updatedJob, 'RUNNING');
+    jobCollection[updatedJob.id] = updatedJob;
+    await genUpdateJob(updatedJob);
+    try {
+      await genRunJob(updatedJob);
+    } catch (error) {
       ERROR('JOB-RUNNER', `Error when executing job: ${error.toString()}`);
-    });
-    delete jobSubscriptions[jobID];
-  }, runAtMillis - nowMillis);
+      updatedJob = updateJobStatus(updatedJob, 'FAILURE');
+      jobCollection[updatedJob.id] = updatedJob;
+      await genUpdateJob(updatedJob);
+      delete jobSubscriptions[updatedJob.id];
+      return;
+    }
+    updatedJob = updateJobStatus(updatedJob, 'SUCCESS');
+    jobCollection[updatedJob.id] = updatedJob;
+    await genUpdateJob(updatedJob);
+    delete jobSubscriptions[updatedJob.id];
+  }, timeTillRunMillis);
   return {
     remove: () => clearTimeout(timeout),
   };
