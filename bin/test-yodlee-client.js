82612d91b731e44991b743eaad9d9513997895a9
Brendan McNamara <brendan.d.mcnamara@vanderbilt.edu>
2018-05-02 19:19:16 -0700

    minor tweaks to test

--- bin/test-yodlee-client.js
+++ bin/test-yodlee-client.js
@@ -1,7 +1,7 @@
 
 /* eslint-disable no-console */
 
-const COBRAND_LOGIN_NAME = 'sbCobbrenmcnamara'
+const COBRAND_LOGIN_NAME = 'sbCobbrenmcnamara';
 const COBRAND_PASSWORD = 'd19ced89-5e46-43da-9b4f-cd5ba339d9ce';
 const COBRAND_LOCALE = 'en_US';
 
@@ -15,7 +15,7 @@ let promise = Promise.resolve();
 
 promise = promise
   .then(() => {
-    console.log(chalk.blue('\n--- TESTING COBRAND LOGIN ---\n'));
+    console.log('\n--- TESTING COBRAND LOGIN ---');
   })
   .then(() => {
     return YodleeClient.genCobrandAuth(COBRAND_LOGIN_NAME, COBRAND_PASSWORD, COBRAND_LOCALE);
@@ -27,6 +27,8 @@ promise = promise
     console.log(chalk.red('Cobrand login failed'));
     process.exit(1);
   })
+
+  // TODO: Test cobrand login failure.
   .then(() => {
     process.exit(0);
   });
