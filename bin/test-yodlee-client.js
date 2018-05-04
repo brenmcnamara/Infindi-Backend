e8a4cf9ac0d09ac4b5666b9a1786a883d46c2b59
Brendan McNamara <brendan.d.mcnamara@vanderbilt.edu>
2018-05-03 20:59:03 -0700

    Adds cobrand login failure test

--- bin/test-yodlee-client.js
+++ bin/test-yodlee-client.js
@@ -1,9 +1,12 @@
 
-/* eslint-disable no-console */
+/* eslint-disable no-console, max-len */
 
 const COBRAND_LOGIN_NAME = 'sbCobbrenmcnamara';
 const COBRAND_PASSWORD = 'd19ced89-5e46-43da-9b4f-cd5ba339d9ce';
 const COBRAND_LOCALE = 'en_US';
+
+const COBRAND_LOGIN_NAME_BAD = 'blah';
+const COBRAND_PASSWORD_BAD = 'blah';
 
 const YodleeClient = require('../build/yodlee/YodleeClient-V1.1').default;
 
@@ -14,6 +17,19 @@ const chalk = require('chalk');
 let promise = Promise.resolve();
 
 promise = promise
+  .then(() => {
+    console.log('\n--- TESTING COBRAND LOGIN FAILURE ---');
+  })
+  .then(() => {
+    return YodleeClient.genCobrandAuth(COBRAND_LOGIN_NAME_BAD, COBRAND_PASSWORD_BAD, COBRAND_LOCALE);
+  })
+  .then(() => {
+    console.log(chalk.red('Expected cobrand login failure to throw'));
+  })
+  .catch(error => {
+    console.log(chalk.green('Cobrand auth failure detection was successful'));
+  })
+
   .then(() => {
     console.log('\n--- TESTING COBRAND LOGIN ---');
   })
@@ -26,9 +42,4 @@ promise = promise
   .catch(error => {
     console.log(chalk.red('Cobrand login failed'));
     process.exit(1);
-  })
-
-  // TODO: Test cobrand login failure.
-  .then(() => {
-    process.exit(0);
   });
