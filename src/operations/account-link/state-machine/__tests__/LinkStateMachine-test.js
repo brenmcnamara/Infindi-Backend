368131dae5ff5533f91a2ebacd641e2182005991
Brendan McNamara <brendan.d.mcnamara@vanderbilt.edu>
2018-05-14 20:33:40 -0700

    Adds first test for link state machine

--- src/operations/account-link/state-machine/__tests__/LinkStateMachine-test.js
+++ src/operations/account-link/state-machine/__tests__/LinkStateMachine-test.js
@@ -1,5 +1,21 @@
 /* @flow */
 
-test('two plus two', () => {
-  expect(2 + 2).toBe(4);
+import InitializingLinkState from '../InitializingLinkState';
+// import LinkEngine from '../LinkEngine';
+import LinkStateMachine from '../LinkStateMachine';
+
+jest.mock('../LinkEngine', () => ({
+  genRefetchAccountLink: jest.fn(),
+  genRefreshAccountLink: jest.fn(),
+  genSetAccountLink: jest.fn(),
+  onLinkEvent: jest.fn(),
+}));
+
+test('starts at initial link state by default', () => {
+  const machine = new LinkStateMachine(
+    '0',
+    'IN_PROGRESS / INITIALIZING',
+    'MANUAL',
+  );
+  expect(machine.getCurrentState()).toBeInstanceOf(InitializingLinkState);
 });
