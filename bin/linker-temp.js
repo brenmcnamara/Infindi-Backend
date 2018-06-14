792df86ac13b1022a6413f6f923aec80d962154e
Brendan McNamara <brendan.d.mcnamara@vanderbilt.edu>
2018-06-13 21:34:53 -0700

    Adds more linker ids to test

--- bin/linker-temp.js
+++ bin/linker-temp.js
@@ -5,16 +5,24 @@ const LinkEngine = require('../build/operations/account-link/state-machine/LinkE
 const LinkStateMachine = require('../build/operations/account-link/state-machine/LinkStateMachine').default;
 const YodleeManager = require('../build/yodlee/yodlee-manager');
 
-const CHASE = 'bc54e92e-1128-4671-bf5d-0d80af7c011a';
-const BANK_OF_AMERICA = 'aa9be048-21c9-488f-9af9-570d59bde24d';
-const BARCLAYCARD = '0cbf98e7-3512-436d-8eec-fc580457155d';
-const FIDELITY = '5d028a07-e930-4aed-b235-1985bcef6412';
-const VANGUARD = '6e1d1bbf-f1e7-4150-911b-64e0785b2237';
-const WELLS_FARGO = '76fe9160-a415-4693-b8ff-1ea09153e332';
+const BMAC = {
+  CHASE: 'bc54e92e-1128-4671-bf5d-0d80af7c011a',
+  BANK_OF_AMERICA: 'aa9be048-21c9-488f-9af9-570d59bde24d',
+  BARCLAYCARD: '0cbf98e7-3512-436d-8eec-fc580457155d',
+  FIDELITY: '5d028a07-e930-4aed-b235-1985bcef6412',
+  VANGUARD: '6e1d1bbf-f1e7-4150-911b-64e0785b2237',
+  WELLS_FARGO: '76fe9160-a415-4693-b8ff-1ea09153e332',
+};
 
-const engine = new LinkEngine(BANK_OF_AMERICA);
+const ALFI = {
+  VANGUARD: '2e740da3-c800-43f4-9d7b-a9b75fbd60ca',
+  VANGUARD_RETIREMENT: '4dd72668-d4b7-428c-b1bb-0edc31f3877d',
+  WELLS_FARGO: 'abb6f8d2-8847-4b3d-893f-276f2ba94959',
+};
+
+const engine = new LinkEngine(ALFI.WELLS_FARGO);
 const machine = new LinkStateMachine({
-  accountLinkID: BANK_OF_AMERICA,
+  accountLinkID: ALFI.WELLS_FARGO,
   engine: engine,
   mode: 'FOREGROUND_UPDATE',
   shouldForceLinking: true,
