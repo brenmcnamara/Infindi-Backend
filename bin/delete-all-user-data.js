787c754761970e21f48d4000bd65b1ccf1926612
Brendan McNamara <brendan.d.mcnamara@vanderbilt.edu>
2018-08-01 19:32:48 -0700

    Adds script to delete all user data

---
+++ bin/delete-all-user-data.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-console */
+
+require('./cli-setup');
+
+const AccountLinkFetcher = require('common/lib/models/AccountLinkFetcher').default;
+const AccountLinkQuery = require('common/lib/models/AccountLinkQuery').default;
+const FirebaseAdmin = require('firebase-admin');
+const FindiError = require('common/lib/FindiError').default;
+
+const chalk = require('chalk');
+const minimist = require('minimist');
+const performDeleteLink = require('../build/operations/account-link/performDeleteLink').default;
+
+const argv = minimist(process.argv.slice(2));
+const userID = argv.userID;
+
+// -----------------------------------------------------------------------------
+//
+// VALIDATE PARAMETERS
+//
+// -----------------------------------------------------------------------------
+
+if (!userID) {
+  console.log(chalk.red('You must provide argument --userID=<userID>'));
+  process.exit(1);
+}
+
+// -----------------------------------------------------------------------------
+//
+// SCRIPT STARTS HERE
+//
+// -----------------------------------------------------------------------------
+
+genValidateUserExists(userID)
+  .then(() => AccountLinkFetcher.genQuery(AccountLinkQuery.forUser(userID)))
+  .then(accountLinks => {
+    accountLinks.forEach(accountLink => {
+      performDeleteLink(accountLink.id);
+    });
+  })
+  .catch(error => {
+    const findiError = FindiError.fromUnknownEntity(error);
+    console.log(chalk.red(findiError.toString()));
+    process.exit(1);
+  });
+
+// -----------------------------------------------------------------------------
+//
+// UTILITIES
+//
+// -----------------------------------------------------------------------------
+
+function genValidateUserExists(userID) {
+  return FirebaseAdmin.firestore()
+    .collection('UserInfo')
+    .doc(userID)
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        console.log(chalk.red('No user found with id:', userID));
+        process.exit(1);
+      }
+    });
+}
